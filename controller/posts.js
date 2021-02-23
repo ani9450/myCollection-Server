9f540897c546041f524a1b8b4ae255da8374073c
Aniket <aniketrai9450@gmail.com>
2021-02-23 19:29:50 +0530

    Enhanced the application with JWT authentication

--- controller/posts.js
+++ controller/posts.js
@@ -5,20 +5,19 @@ import PostMessage from '../model/postMessage.js';
 export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
-
-
+        console.log()
         res.status(200).json(postMessage)
     }
     catch (err) {
-        res.status(404).json({ message: err.message })
-            ;
+        res.status(404).json({ message: err.message });
+
     }
 };
 
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     try {
         await newPost.save();
         res.status(201).json(newPost);
@@ -31,19 +30,16 @@ export const createPost = async (req, res) => {
 }
 export const updatePost = async (req, res) => {
     try {
+        const { id } = req.params;
+        const { title, message, creator, selectedFile, tags } = req.body;
 
-        const { id: _id } = req.param;
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-        const post = req.body
-        console.log("updatePost id -- server-- ", id);
+        const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-        if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No post with that id' })
+        await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
-        console.log("updatePost id 2-- server-- ", id);
-
-        const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
-
-        res.status(200).json(updatedPost)
+        res.json(updatedPost);
     }
     catch (err) {
         res.status(404).json({ message: err.message });
@@ -64,10 +60,21 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if (!req.userId) return res.json({ message: "Unauthenticated" });
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: 'No post with that id' })
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
     res.json({ message: "updated post successfully !!" });
 
